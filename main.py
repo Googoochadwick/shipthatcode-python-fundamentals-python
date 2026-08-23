5bff51ed49ce5b5d41e9f2566b8b20c9c3565c52
googoochadwick <arush.anand.singh.3121@gmail.com>
2026-08-24 00:46:02 +0530

    lesson 9

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-n = int(input())
-# Print the multiplication table from 1 to 10
-for i in range(1,11):
-    print(f"{n} x {i} = {n*i}")
+total = 0
+# Read numbers and accumulate them until you see 0
+while True:
+    a = int(input())
+    if a != 0:
+        total += a
+    else:
+        print(total)
+        break
