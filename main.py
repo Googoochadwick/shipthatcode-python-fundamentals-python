ec21f81f04b2943cb8b463b475d83c6939a154c8
googoochadwick <arush.anand.singh.3121@gmail.com>
2026-08-24 08:59:00 +0530

    lesson 10

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-total = 0
-# Read numbers and accumulate them until you see 0
-while True:
-    a = int(input())
-    if a != 0:
-        total += a
-    else:
-        print(total)
-        break
+n = int(input())
+# Print a right-aligned triangle of stars
+for i in range (1,n+1):
+    print(" "*(n-i), end="")
+    print("*"*i)
